621e070dad675206d64f5a26d15114fccfc97975
IvanNLarionov <il@contuur.com>
2019-09-16 23:49:16 +0300

    added rest service

---
+++ rest_service.py
@@ -0,0 +1,34 @@
+from flask import Flask
+import json
+
+DATA_EXAMPLE = [
+    {'name': 'БАНК КРЕМЛЕВСКИЙ ОКВКУ Аврора',
+     'phone': '+7 903 575-11-83',
+     'buy': '64.50', 'sell': '61.50',
+     'time': '23:34', 'address': 'ул. Петровка, дом 11'},
+    {'name': 'БАНК КРЕМЛЕВСКИЙ ОКВКУ Лесная',
+     'phone': '+7 968 534-76-17',
+     'buy': '64.70', 'sell': '61.50', 'time': '23:34', 'address': 'Улица Лесная, д. 15'}]
+
+
+def process_request():
+    return DATA_EXAMPLE
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/load', methods=['GET'])
+def calculate_method():
+    data = process_request()
+
+    response = app.response_class(
+            response=json.dumps(data, ensure_ascii=False),
+            status=200,
+            mimetype='application/json')
+    return response
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
