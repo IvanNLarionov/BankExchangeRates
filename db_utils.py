964e8ce7d8f16cdaf5ba39811e6b0ecc72f7b00d
IvanNLarionov <il@contuur.com>
2019-09-22 20:15:15 +0300

    debugging

--- db_utils.py
+++ db_utils.py
@@ -4,25 +4,25 @@ import mysql.connector
 def insert_row(object, cursor):
     sql = f"""INSERT INTO office_rates (load_id, office_name, office_phone, time, buy_rate, sell_rate, address, longitude, latitude)""" \
         f""" VALUES ({object['load_id']}, '{object['office_name']}', '{object['office_phone']}', '{object['time']}', """ \
-        f"""{object['buy_rate']}, {object['sell_rate']}, '{object['address']}', {object['longitude']}, {object[
-            'latitude']})"""
+        f"""{object['buy_rate']}, {object['sell_rate']}, '{object['address']}', {object['longitude']}, {object['latitude']})"""
     cursor.execute(sql)
 
 
 def update_db(results):
-    with mysql.connector.connect(
+    conn = mysql.connector.connect(
             database="mvpdb",
             host="localhost",
             user="dbuser",
-            passwd="12345") as conn:
+            passwd="12345")
 
-        cursor = conn.cursor(buffered=True)
+    cursor = conn.cursor(buffered=True)
 
-        _ = cursor.execute("SELECT MAX(load_id) from office_rates")
-        last_load_id = cursor.fetchone()[0]
+    _ = cursor.execute("SELECT MAX(load_id) from office_rates")
+    last_load_id = cursor.fetchone()[0]
 
-        for item in results:
-            item['load_id'] = last_load_id + 1
-            insert_row(item, cursor)
+    for item in results:
+        item['load_id'] = last_load_id + 1
+        insert_row(item, cursor)
 
-        conn.commit()
+    conn.commit()
+    conn.close()
