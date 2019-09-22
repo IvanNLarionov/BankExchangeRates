36643c91d366d0958ed6730036511b68e986e27b
IvanNLarionov <il@contuur.com>
2019-09-22 20:12:54 +0300

    updated with db

---
+++ db_utils.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+
+def insert_row(object, cursor):
+    sql = f"""INSERT INTO office_rates (load_id, office_name, office_phone, time, buy_rate, sell_rate, address, longitude, latitude)""" \
+        f""" VALUES ({object['load_id']}, '{object['office_name']}', '{object['office_phone']}', '{object['time']}', """ \
+        f"""{object['buy_rate']}, {object['sell_rate']}, '{object['address']}', {object['longitude']}, {object[
+            'latitude']})"""
+    cursor.execute(sql)
+
+
+def update_db(results):
+    with mysql.connector.connect(
+            database="mvpdb",
+            host="localhost",
+            user="dbuser",
+            passwd="12345") as conn:
+
+        cursor = conn.cursor(buffered=True)
+
+        _ = cursor.execute("SELECT MAX(load_id) from office_rates")
+        last_load_id = cursor.fetchone()[0]
+
+        for item in results:
+            item['load_id'] = last_load_id + 1
+            insert_row(item, cursor)
+
+        conn.commit()
---
+++ scrape_utils.py
@@ -0,0 +1,74 @@
+import logging
+
+from bs4 import BeautifulSoup
+
+OFFICE_CLASS = "quote__office__one js-one-office"
+NAME_CLASS = "quote__office__one__name"
+BUY_CLASS = "quote__office__cell quote__office__one__buy js-filter-pro quote__mode_list_view_pro"
+SELL_CLASS = "quote__office__cell quote__office__one__sell js-filter-pro quote__mode_list_view_pro"
+PHONE_CLASS = "quote__office__one__phone"
+TIME_CLASS = "quote__office__cell quote__office__one__time"
+
+BASE_URL = 'https://cash.rbc.ru'
+
+
+def get_offices(soup):
+    return soup.findAll("div", {"class": OFFICE_CLASS})
+
+
+def get_office_name(office_soup):
+    return office_soup.find("a", {"class": NAME_CLASS}).text
+
+
+def get_office_ref(office_soup):
+    return BASE_URL + office_soup.find("a", {"class": NAME_CLASS})['href']
+
+
+def get_phone(office_soup):
+    return office_soup.find("div", {"class": PHONE_CLASS}).text
+
+
+def get_buy_rate(office_soup):
+    return list(office_soup.find("div", {"class": BUY_CLASS}).stripped_strings)[0]
+
+
+def get_sell_rate(office_soup):
+    return list(office_soup.find("div", {"class": SELL_CLASS}).stripped_strings)[0]
+
+
+def get_record_time(office_soup):
+    return office_soup.find("div", {"class": TIME_CLASS}).text.replace("\n", "").replace(" ", "")
+
+
+async def fetch_inner(office_ref, session):
+
+    async with session.get(office_ref) as response:
+        text = await response.text()
+        try:
+            soup = BeautifulSoup(text, 'html.parser')
+            address = soup.find("div", {"class": "quote__info__address"}).text
+            address = address.replace("Адрес: ", "")
+            return address
+        except Exception as e:
+            logging.debug(e.args)
+            return None
+
+
+async def create_office_record(office_soup, session):
+    name = get_office_name(office_soup)
+    phone = get_phone(office_soup)
+    buy_rate = get_buy_rate(office_soup)
+    sell_rate = get_sell_rate(office_soup)
+    time = get_record_time(office_soup)
+
+    inner_ref = get_office_ref(office_soup)
+    address = await fetch_inner(inner_ref, session)
+
+    return {'name': name,
+            'phone': phone,
+            'buy': buy_rate,
+            'sell': sell_rate,
+            'time': time,
+            'address': address,
+            'longitude': 55.7558,
+            'latitude': 37.6273}
